446f0b3b224c9c3c03c01dab95b58446d97bfb1f
Aman Kumar <amanrajput9871506467@gmail.com>
2026-04-06 16:19:48 +0530

    All EXP

---
+++ JavaScript/JS fundamentals/App.js
@@ -0,0 +1,53 @@
+// let age=12;
+// const self="Aman";
+// const pi=3.14;
+// var name="Aman";
+// var Isemployed = false;
+// var name="Aditya";
+
+
+// let age=12;
+// const self="Aman";
+// const pi=3.14;
+// const Isemployed = false;
+// let name="Aditya";
+// console.log(name);
+// console.log(age);
+// console.log(self);
+// console.log(pi);
+// console.log(Name);
+
+// if(true){
+//     var age=30;
+// }
+// console.log(age);
+
+
+// if(true){
+//     let age=30;
+// }
+// console.log(age);
+
+// const pi=3.14;
+// console.log(pi);
+// pi=3.1456;
+// console.log(pi);
+
+// console.log(typeof name);
+// console.log(typeof age)
+// console.log(typeof self)
+// console.log(typeof pi)
+
+
+// let name="Aman kumar";
+// let age=19;
+// console.log("My name is " +name+ " and my age is "+age);
+// console.log(`My name is ${name} and my age is ${age}`);
+
+let Data=null;
+console.log(Data);
+console.log(typeof Data);
+
+let result;
+console.log(result);
+console.log(typeof result);
